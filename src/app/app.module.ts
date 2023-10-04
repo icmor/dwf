0aa5df474a95d1e73b7f1c705445ed795cd6049f
icmor <cornejodlm@ciencias.unam.mx>
2023-10-03 21:49:18 -0600

    Product module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProductModule } from './modules/product/product.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
