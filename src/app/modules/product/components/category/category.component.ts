003e7eeb40b21ffb2757f2e8ba6745be5f9abc4e
icmor <cornejodlm@ciencias.unam.mx>
2023-10-10 23:11:13 -0600

    category component modal form

--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
+import { FormBuilder, Validators } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'app-category',
@@ -7,12 +10,24 @@ import { Category } from '../../_models/category';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent {
-
-    ngOnInit(){
-	this.getCategories();
-    }
   categories: Category[] = [];
 
+  form = this.formBuilder.group({
+    category: ["", [Validators.required]],
+    code: ["", [Validators.required]],
+  });
+
+  submitted = false;
+  index = 0;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categories.push(
       new Category(1, "0111161", "The Shawshank Redemption", "9.3/10")
     );
     this.categories.push(
@@ -21,4 +36,25 @@ export class CategoryComponent {
     this.categories.push(
       new Category(3, "0468569", "The Dark Knight", "9.0/10")
     );
+
+    this.index += 3;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.form.invalid) return;
+    this.submitted = false;
+
+    let category = new Category(this.index++, this.form.controls['code'].value!,
+				this.form.controls['category'].value!, "10/10");
+    this.categories.push(category);
+    $("#modalForm").modal("hide");
+    alert("Categoría guardada exitósamente!");
+  }
+
+  showModalForm(){
+    this.form.reset();
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
 }
