8369be036789d827f479fda06a9af6c09cf992f7
icmor <cornejodlm@ciencias.unam.mx>
2023-12-11 23:42:56 -0600

    Customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './modules/product/components/product/product.component';
 import { ProductListComponent } from './modules/product/components/product-list/product-list.component';
 import { CategoryComponent } from './modules/category/components/category/category.component';
+import { CustomerComponent } from './modules/customer/components/customer/customer.component';
+import { CustomerImageComponent } from './modules/customer/components/customer-image/customer-image.component';
+import { RegionComponent } from './modules/customer/components/region/region.component';
+import { InvoiceComponent } from './modules/invoice/components/invoice/invoice.component';
+import { InvoiceListComponent } from './modules/invoice/components/invoice-list/invoice-list.component';
+import { CartComponent } from './modules/invoice/components/cart/cart.component';
 
 const routes: Routes = [
   { path: "products", component: ProductListComponent },
   { path: "categories", component: CategoryComponent },
   {path: 'product', redirectTo: 'product/', pathMatch: 'full'},
   { path: "product/:gtin", component: ProductComponent },
+  { path: "customer/:rfc", component: CustomerImageComponent },
+  { path: "customers", component: CustomerComponent },
+  { path: "regions", component: RegionComponent },
+  { path: "invoices", component: InvoiceListComponent},
+  { path: "invoice/:id", component: InvoiceComponent},
+  { path: "cart/:gtin", component: CartComponent },
+  { path: "cart", component: CartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {NgxPhotoEditorModule} from "ngx-photo-editor";
+import { NgxPhotoEditorModule } from "ngx-photo-editor";
+import { QRCodeModule } from 'angularx-qrcode';
 
 import { ProductModule } from './modules/product/product.module';
+import { InvoiceModule } from './modules/invoice/invoice.module';
 import { CategoryModule } from './modules/category/category.module';
 import { LayoutModule } from './modules/layout/layout.module';
+import { CustomerModule } from './modules/customer/customer.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,8 +21,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     ProductModule,
     CategoryModule,
+    CustomerModule,
+    InvoiceModule,
     LayoutModule,
-    NgxPhotoEditorModule
+    NgxPhotoEditorModule,
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
